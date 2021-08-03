9a84b8f82f60faede2e233330ae795db400bff65
Abhishek Guha <abhi.workspace@gmail.com>
2021-08-03 22:49:21 +0200

    Find all subsets

---
+++ leetcode/subset2/subset2.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/subsets-ii/
+from typing import List, Tuple
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[Tuple[int]]:
+        n = len(nums)
+        result = set()
+        nums.sort()
+        for i in range(2**n):
+            ans = []
+            for j in range(n):
+                if i & (1<<j):
+                    ans.append(nums[j])
+            result.add(tuple(ans))
+        return list(result)
+
+
+def check(solution, test: List[int], ans: List[Tuple[int]]) -> None:
+    assert solution.subsetsWithDup(test) == [(), (2,)], "test failed :("
+
+
+def main():
+    solution = Solution()
+
+    test = [2]
+    ans = [(), (2,)]
+
+    check(solution, test, ans)
+    print("ok")
+
+
+if __name__ == "__main__":
+    main()
