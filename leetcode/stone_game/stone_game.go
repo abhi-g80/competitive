cf409da4efe3b4bcd6c0aeedaeaa7860bb4d8d5c
Abhishek Guha <abhi.workspace@gmail.com>
2021-08-05 22:51:06 +0200

    Easy win

---
+++ leetcode/stone_game/stone_game.go
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/stone-game/
+// Alex always wins because
+// - the total number of stones in the pile are odd
+// - the length of array is even
+// - alex can take all the even numbered or odd numbered stones
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    tests := []int{4,3,2,3}
+
+    if stoneGame(tests) == true  {
+        fmt.Println("ok")
+    }
+}
+
+func stoneGame(piles []int) bool {
+    return true
+}
