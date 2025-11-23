d14cc6da3ac4b14cef98ac5064759d99ef5da77e
Abhishek Guha <abhi.workspace@gmail.com>
2025-11-23 21:18:45 +0100

    feat: Code for dumb twitter

---
+++ src/neetcode_design_twitter_feed.cpp
@@ -0,0 +1,132 @@
+// https://neetcode.io/problems/design-twitter-feed
+#include <cassert>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Twitter {
+    vector<vector<int>> followers;
+    vector<vector<pair<int, int>>> tweets;
+    int tweet_ts;
+
+   public:
+    Twitter() {
+        for (int i = 0; i < 100; i++) tweets.push_back(vector<pair<int, int>>{});
+        for (int i = 0; i < 100; i++) followers.push_back(vector<int>{});
+        tweet_ts = 0;
+    }
+
+    void postTweet(int userId, int tweetId) {
+        this->tweets[userId].push_back({tweetId, tweet_ts++});
+    }
+
+    vector<int> getNewsFeed(int userId) {
+        auto comp = [](pair<int, int>& a, pair<int, int>& b) { return a.second < b.second; };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(
+            comp);  // max heap
+        for (auto& x : tweets[userId]) pq.push(x);
+
+        vector<int> ufollowers = followers[userId];
+
+        for (int& x : ufollowers) {
+            if (x < 0) continue;
+            for (auto& n : tweets[x]) pq.push(n);
+        }
+        vector<int> tweetItems;
+        while (!pq.empty() && tweetItems.size() < 10) {
+            tweetItems.push_back(pq.top().first);
+            pq.pop();
+        }
+        return tweetItems;
+    }
+
+    void follow(int followerId, int followeeId) {
+        if(followerId == followeeId) return;
+        for(auto& x: this->followers[followerId]) if(x == followeeId) return;
+        this->followers[followerId].push_back(followeeId);
+    }
+
+    void unfollow(int followerId, int followeeId) {
+        vector<int> ufollowers = this->followers[followerId];
+        uint32_t idx = 0;
+        for (uint32_t i = 0; i < ufollowers.size(); i++) {
+            if (ufollowers[i] == followeeId) {
+                idx = i;
+                break;
+            }
+        }
+        this->followers[followerId][idx] = -1;
+    }
+};
+enum Commands { PostTweet, GetNewsFeed, Follow, Unfollow };
+
+struct TestCase {
+    vector<int>& commands;
+    vector<vector<int>>& values;
+    Twitter* t;
+
+    void run() {
+        assert(values.size() == commands.size());
+
+        int userId, tweetId, followerId, followeeId;
+        vector<int> feed;
+        for (uint32_t i = 0; i < commands.size(); i++) {
+            switch (commands[i]) {
+                case PostTweet:
+                    userId = values[i][0];
+                    tweetId = values[i][1];
+                    t->postTweet(userId, tweetId);
+                    cout << "User(" << userId << ") posted tweet: " << tweetId << "\n";
+                    break;
+                case GetNewsFeed:
+                    userId = values[i][0];
+                    feed = t->getNewsFeed(userId);
+                    cout << "NewsFeed user(" << userId << "): ";
+                    for (int x : feed) cout << x << " ";
+                    cout << "\n";
+                    break;
+                case Follow:
+                    followerId = values[i][0];
+                    followeeId = values[i][1];
+                    t->follow(followerId, followeeId);
+                    cout << "User(" << followerId << ") started following User(" << followeeId
+                         << ")\n";
+                    break;
+                case Unfollow:
+                    followerId = values[i][0];
+                    followeeId = values[i][1];
+                    t->unfollow(followerId, followeeId);
+                    cout << "User(" << followerId << ") stopped following User(" << followeeId
+                         << ")\n";
+                    break;
+                default:
+                    cout << "Wrong command\n";
+            }
+        }
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    Twitter* t = new Twitter();
+
+    vector<int> commands = vector<int>{PostTweet,   PostTweet,   GetNewsFeed, GetNewsFeed, Follow,
+                                       GetNewsFeed, GetNewsFeed, Unfollow,    GetNewsFeed, Follow, Follow, GetNewsFeed};
+
+    vector<vector<int>> values =
+        vector<vector<int>>{vector<int>{1, 10}, vector<int>{2, 20}, vector<int>{1},
+                            vector<int>{2},     vector<int>{1, 2},  vector<int>{1},
+                            vector<int>{2},     vector<int>{1, 2},  vector<int>{1}, {1, 2}, {1, 2}, {1}};
+
+    vector<TestCase> testcases = {
+        {commands, values, t},
+    };
+
+    for (auto& x : testcases) {
+        x.run();
+    }
+
+    return 0;
+}
